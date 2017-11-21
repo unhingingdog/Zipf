ef1d3e92948eb18c6718118c89d136022b7cccfb
HAMISH <constatcopywriting@gmail.com>
2017-11-21 20:19:53 +1300

    still havent configed unit tests with enzyme. Trying from scratch

--- __tests__/TextEditor.test.js
+++ __tests__/TextEditor.test.js
@@ -1,11 +1,13 @@
 import Adapter from 'enzyme-adapter-react-16'
 import TextEditor from '../src/components/TextEditor'
 import React from 'react';
-import Enzyme, { shallow } from 'enzyme';
+import 'react-dom'
+import Enzyme, { shallow, render, mount } from 'enzyme';
 Enzyme.configure({ adapter: new Adapter() })
 import { View, Text, StyleSheet, Button } from 'react-native';
 import chai, { expect } from 'chai';
 import sinon from 'sinon'
+let should = chai.should()
 
 describe('<TextEditor /> renders', () => {
 	let wrapper
@@ -14,13 +16,10 @@ describe('<TextEditor /> renders', () => {
     expect(wrapper.length).to.equal(1)
   })
 	it('it should contain text', () => {
-		expect(wrapper.contains(<Text>Hello?</Text>)).to.equal(true);
+		expect(wrapper.contains(<Text>Hello?</Text>)).to.equal(true)
 	})
 	it('it should contain a button', () => {
 		expect(wrapper.contains(<Button title="some shit" onPress={this.pressHandler}></Button>)).to.equal(true)
-	})
-	it('changes the text when the button is pressed', () => {
-			
 	})
 })
 
@@ -28,6 +27,11 @@ describe('<TextEditor /> interactions', () => {
 	const onButtonClick = sinon.spy()
 	let wrapper = shallow(<TextEditor onPress={onButtonClick} test="hello?" />)
 	wrapper.find('Button').simulate('click')
-	expect(--reactnative click shit--)
 })
+
+describe('other testing', () => {
+	beforeEach(() => {
 		
+	})
+})
+
