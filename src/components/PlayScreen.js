71af5dfc3e3d491873134e7fe43070686db30162
HAMISH <constatcopywriting@gmail.com>
2018-02-09 09:47:52 +1300

    Added pause button functionality. Working.

--- src/components/PlayScreen.js
+++ src/components/PlayScreen.js
@@ -3,20 +3,30 @@ import { View, Text, Button } from 'react-native'
 import Word from './Word'
 import Redux from 'redux'
 import { connect } from 'react-redux'
-import { PlayAction, IncrementAction, StopAction } from '../actions/PlayAction'
+import {
+	IncrementAction,
+	PlayAction,
+	PauseAction,
+	StopAction
+} from '../actions/PlayAction'
 
 //exported so it can be tested without mocking store
 export class PlayScreen extends Component {
 	constructor(props) {
 		super(props)
-		console.log('constructor fired')
-		this.playPressHandler = this.playPressHandler.bind(this)
-		this.stopPressHandler = this.stopPressHandler.bind(this)
-		this.playWord         = this.playWord.bind(this)
+
+		this.playPressHandler  = this.playPressHandler.bind(this)
+		this.pausePressHandler = this.pausePressHandler.bind(this)
+		this.stopPressHandler  = this.stopPressHandler.bind(this)
+		this.playWord          = this.playWord.bind(this)
 	}
 
 	playPressHandler() {
 		this.props.PlayAction()
+	}
+
+	pausePressHandler() {
+		this.props.PauseAction()
 	}
 
 	stopPressHandler() {
@@ -24,55 +34,31 @@ export class PlayScreen extends Component {
 	}
 
 	playWord(interval, playing, action) {
-		console.log(interval, this.props.feed[interval])
 		this.countdown = setInterval(action, interval)
 	}
 
-	componentWillMount() {
-		console.log('component will mount')
-	}
-
-	componentDidMount() {
-		console.log('component did mount')
-	}
-
-	componentWillReceiveProps(nextProps) {
-		console.log('component receiving props')
-		console.log(nextProps.place)
-	}
-
-	shouldComponentUpdate() {
-		console.log('should component update')
-		return true
-	}
-
-	componentWillUpdate(nextProps, nextState) {
-		console.log('component will update')
+	componentWillUpdate() {
 		clearInterval(this.countdown)
-		console.log(nextProps.playing)
 	}
 
 	componentDidUpdate() {
-		console.log('component updated')
 		const { place, feed, IncrementAction, playing } = this.props
 		const interval = feed[place].displayTime
 		if (this.props.playing) this.playWord(interval, playing, IncrementAction)
 	}
 
-
 	componentWillUnmount() {
-		console.log('component unmounted')
-		// this.props.StopAction()
-		// clearInterval(this.countdown)
+		clearInterval(this.countdown)
+		this.props.StopAction()
 	}
 
 	render() {
-		console.log('rendering')
 		const { place, feed } = this.props
 		return(
 			<View>
 				<Text>{feed[place].word}</Text>
 				<Button title="play" onPress={this.playPressHandler}></Button>
+				<Button title="pause" onPress={this.pausePressHandler}></Button>
 				<Button title="stop" onPress={this.stopPressHandler}></Button>
 			</View>
 		)
@@ -89,6 +75,7 @@ const mapStateToProps = state => {
 
 export default connect(mapStateToProps, {
 	PlayAction,
+	PauseAction,
 	StopAction,
 	IncrementAction
 })(PlayScreen)
