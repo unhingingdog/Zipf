958f89e9a4929e978acf83954d97b69f6f5b7df0
HAMISH <constatcopywriting@gmail.com>
2018-02-27 19:06:28 +1300

    removed nachos-ui slider, as it did not have onSlidingComplete prop. About to extract words in Paused into seperate component.

--- src/components/Paused.js
+++ src/components/Paused.js
@@ -8,7 +8,7 @@ import {
   TouchableOpacity
 } from 'react-native'
 import { Ionicons } from '@expo/vector-icons'
-import { Segment } from 'native-base'
+
 
 export default class PausedMode extends Component {
   constructor(props) {
